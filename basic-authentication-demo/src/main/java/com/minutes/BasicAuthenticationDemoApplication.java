9f794c75ba0d133165e503b83a031025463536f0
suchita <suchita.k.mahajan@gmail.com>
2018-12-04 16:57:45 +0530

    commit for basic authentication

---
+++ basic-authentication-demo/src/main/java/com/minutes/BasicAuthenticationDemoApplication.java
@@ -0,0 +1,12 @@
+package com.minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthenticationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthenticationDemoApplication.class, args);
+	}
+}
